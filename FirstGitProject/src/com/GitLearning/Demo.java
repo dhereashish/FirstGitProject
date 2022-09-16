588982057eb35115bf864af74b5e72809f7c22f1
dhereashish <dhereashish15@gmail.com>
2022-09-16 13:50:12 +0530

    First Arralist

---
+++ FirstGitProject/src/com/GitLearning/Demo.java
@@ -0,0 +1,16 @@
+package com.GitLearning;
+
+import java.util.ArrayList;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		ArrayList<String> al = new ArrayList<String>();
+		al.add("Ashish");
+		al.add("Dhere");
+		
+		System.out.println(al);
+		
+	}
+
+}
