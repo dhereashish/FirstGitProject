bb5cf36638968fadfcb4442691de8b5d6715d3d0
dhereashish <dhereashish15@gmail.com>
2022-09-16 14:05:15 +0530

    Demo2 remove

--- FirstGitProject/src/com/GitLearning/Demo2.java
+++ FirstGitProject/src/com/GitLearning/Demo2.java
@@ -3,7 +3,7 @@ package com.GitLearning;
 public class Demo2 {
 	public static void main(String[] args) {
 		
-		System.out.println("We Done");
+		
 		System.out.println("Heyyy Man!!! How are you");
 		
 	}
